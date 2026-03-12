0c121cba09c4aad102eca45951463affe16e38ac
syf-atelier <sertorrga10@gmail.com>
2026-03-11 21:09:50 -0300

    agregando cositas

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Exo'],
+            },
+        },
+    },
+    plugins: [],
+};
